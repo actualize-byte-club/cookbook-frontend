da1bb64e6ea3c9f3672251beac9a5e77e9b88043
dani <dani.zaghian@gmail.com>
2022-02-08 13:21:06 -0800

    adds v-bind:src for image tag

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,6 +26,15 @@ export default {
     <h1>All Recipes</h1>
     <div v-for="recipe in recipes" v-bind:key="recipe.id">
       <h2>Title: {{ recipe.title }}</h2>
+      <img v-bind:src="recipe.image_url" alt="" />
+      <p>Ingredients: {{ recipe.ingredients }}</p>
+      <p>Directions: {{ recipe.directions }}</p>
     </div>
   </div>
 </template>
+
+<style>
+img {
+  width: 250px;
+}
+</style>

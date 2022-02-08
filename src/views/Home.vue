acd8cf4e8a261b0afa86c832c1bb78ae2153f6ac
dani <dani.zaghian@gmail.com>
2022-02-08 12:16:49 -0800

    adds real request to api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,32 @@
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      recipes: [
-        { id: 1, title: "Raw Eggs", chef: "Jay Wengrow" },
-        { id: 2, title: "Empty Sandwich", chef: "Jay Wengrow" },
-        { id: 3, title: "Ice", chef: "Peter Jang" },
-        { id: 4, title: "Coffee", chef: "Cheddar" }
-      ]
+      recipes: []
     };
   },
-  created: function () {},
-  methods: {}
+  created: function () {
+    this.indexRecipes();
+  },
+  methods: {
+    indexRecipes: function () {
+      axios.get("http://localhost:3000/recipes").then((response) => {
+        console.log(response.data);
+        this.recipes = response.data;
+      });
+    }
+  }
 };
 </script>
 
 <template>
   <div class="home">
     <h1>All Recipes</h1>
+
     <div v-for="recipe in recipes" v-bind:key="recipe.id">
       <h2>Title: {{ recipe.title }}</h2>
-      <p>Chef: {{ recipe.chef }}</p>
     </div>
   </div>
 </template>

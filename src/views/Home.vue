86ef3568d8bcfee7b467aee32b2a069126c38f36
dani <dani.zaghian@gmail.com>
2022-02-08 11:45:35 -0800

    adds recipe hardcoded array and vfor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,12 @@
 export default {
   data: function () {
     return {
-      message: "Hello World!"
+      recipes: [
+        { id: 1, title: "Raw Eggs", chef: "Jay Wengrow" },
+        { id: 2, title: "Empty Sandwich", chef: "Jay Wengrow" },
+        { id: 3, title: "Ice", chef: "Peter Jang" },
+        { id: 4, title: "Coffee", chef: "Cheddar" }
+      ]
     };
   },
   created: function () {},
@@ -12,6 +17,10 @@ export default {
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>All Recipes</h1>
+    <div v-for="recipe in recipes" v-bind:key="recipe.id">
+      <h2>Title: {{ recipe.title }}</h2>
+      <p>Chef: {{ recipe.chef }}</p>
+    </div>
   </div>
 </template>

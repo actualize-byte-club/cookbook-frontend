b669988dd2bcb79523b10cb92f624f49f1e3c0d0
dani <dani.zaghian@gmail.com>
2022-02-15 11:05:43 -0800

    adds isloggedin functionality

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
+<script>
+export default {
+  data: function () {
+    return {
+      isLoggedIn: !!localStorage.jwt
+    };
+  },
+  watch: {
+    $route: function () {
+      this.isLoggedIn = !!localStorage.jwt;
+    }
+  },
+  methods: {
+    // isLoggedIn: function () {
+    //   return localStorage.getItem("jwt");
+    // }
+  }
+};
+</script>
+
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/signup">Signup</router-link> |
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/logout">Logout</router-link> |
+    <router-link v-if="!isLoggedIn" to="/signup">Signup</router-link> |
+    <router-link v-if="!isLoggedIn" to="/login">Login</router-link> |
+    <router-link v-if="isLoggedIn" to="/logout">Logout</router-link> |
     <router-link to="/recipes">Recipes</router-link> |
     <router-link to="/recipes/new">New Recipe</router-link>
   </div>

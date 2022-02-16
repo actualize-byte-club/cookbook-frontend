ec7bd78e202050d5ed667352838a91050e926488
dani <dani.zaghian@gmail.com>
2022-02-16 10:26:02 -0800

    adds animations

--- src/views/RecipesIndex.vue
+++ src/views/RecipesIndex.vue
@@ -44,15 +44,22 @@ export default {
         {{ recipe.title }}
       </option>
     </datalist>
-    <div v-for="recipe in filteredRecipes" v-bind:key="recipe.id">
-      <h2>{{ recipe.title }}</h2>
-      <router-link v-bind:to="`/recipes/${recipe.id}`"
-        ><img v-bind:src="recipe.image_url" alt=""
-      /></router-link>
-      <p>Ingredients: {{ recipe.ingredients }}</p>
-      <p>Directions: {{ recipe.directions }}</p>
-      <p>Prep time: {{ recipe.friendly_prep_time }}</p>
-      <p>Created {{ relativeDate(recipe.created_at) }}</p>
-    </div>
+
+    <transition-group
+      appear
+      enter-active-class="animate__animated animate__fadeIn"
+      leave-active-class="animate__animated animate__fadeOut"
+    >
+      <div v-for="recipe in filteredRecipes" v-bind:key="recipe.id">
+        <h2>{{ recipe.title }}</h2>
+        <router-link v-bind:to="`/recipes/${recipe.id}`"
+          ><img v-bind:src="recipe.image_url" alt=""
+        /></router-link>
+        <p>Ingredients: {{ recipe.ingredients }}</p>
+        <p>Directions: {{ recipe.directions }}</p>
+        <p>Prep time: {{ recipe.friendly_prep_time }}</p>
+        <p>Created {{ relativeDate(recipe.created_at) }}</p>
+      </div>
+    </transition-group>
   </div>
 </template>

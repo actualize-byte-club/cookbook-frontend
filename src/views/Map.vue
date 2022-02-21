763a7d1f3980ff4cb91038705bbba7cbfd012e5c
dani <dani.zaghian@gmail.com>
2022-02-21 11:08:37 -0800

    adds marker and popup

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -13,6 +13,26 @@ export default {
       zoom: 13 // starting zoom
     });
     console.log(map);
+    // create the popup
+    const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+      "This is a place in Chicago"
+    );
+    // Create a default Marker and add it to the map.
+    const marker1 = new mapboxgl.Marker()
+      .setLngLat([-87.623177, 41.881832])
+      .setPopup(popup) // sets a popup on this marker
+      .addTo(map);
+    console.log(marker1);
+    // create the popup
+    const popup2 = new mapboxgl.Popup({ offset: 25 }).setText(
+      "This is a place in San Francisco"
+    );
+    // Create a default Marker and add it to the map.
+    const marker2 = new mapboxgl.Marker()
+      .setLngLat([-122.431297, 37.773972])
+      .setPopup(popup2) // sets a popup on this marker
+      .addTo(map);
+    console.log(marker2);
   }
 };
 </script>

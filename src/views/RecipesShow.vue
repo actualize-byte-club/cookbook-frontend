d178dd715d51eada70187f46fa2efa9f73cd46a8
dani <dani.zaghian@gmail.com>
2022-03-14 13:03:43 -0700

    adds theme install

--- src/views/RecipesShow.vue
+++ src/views/RecipesShow.vue
@@ -29,14 +29,61 @@ export default {
 
 <template>
   <div class="recipes-show">
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs" data-aos="fade-in">
+      <div class="container">
+        <h2>{{ recipe.title }}</h2>
+      </div>
+    </div>
+    <!-- End Breadcrumbs -->
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <div
+            class="col-lg-6 order-1 order-lg-2"
+            data-aos="fade-left"
+            data-aos-delay="100"
+          >
+            <img v-bind:src="recipe.image_url" class="img-fluid" alt="" />
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+            <h3>
+              Voluptatem dignissimos provident quasi corporis voluptates sit
+              assumenda.
+            </h3>
+            <p class="fst-italic">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <ul>
+              <li>
+                <i class="bi bi-check-circle"></i> Ingredients:
+                {{ recipe.ingredients }}
+              </li>
+              <li>
+                <i class="bi bi-check-circle"></i> Directions:
+                {{ recipe.directions }}
+              </li>
+              <li>
+                <i class="bi bi-check-circle"></i> Prep time:
+                {{ recipe.friendly_prep_time }}
+              </li>
+            </ul>
+            <p>
+              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+              irure dolor in reprehenderit in voluptate
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End About Section -->
+
     <!-- <p>Recipe user id: {{ recipe.user_id }}</p>
     <p>Current user id: {{ currentUserId }}</p> -->
 
-    <h2>{{ recipe.title }}</h2>
-    <img v-bind:src="recipe.image_url" alt="" />
-    <p>Ingredients: {{ recipe.ingredients }}</p>
-    <p>Directions: {{ recipe.directions }}</p>
-    <p>Prep time: {{ recipe.friendly_prep_time }}</p>
     <div v-if="recipe.owner">
       <button>
         <router-link v-bind:to="`/recipes/${recipe.id}/edit`">Edit</router-link>
